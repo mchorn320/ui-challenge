0a78fcc1d58792f62ee0ee195b9c85f886a32fca
Mark Horn <markhorn@Marks-MacBook-Pro.local>
2018-11-28 15:11:37 -0500

    all checkin

--- src/main/java/com/lidp/customer/controllers/CustomersController.java
+++ src/main/java/com/lidp/customer/controllers/CustomersController.java
@@ -1,12 +1,16 @@
 package com.lidp.customer.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,6 +41,24 @@ public class CustomersController {
 	@GetMapping("/{id}")
 	public Customer get(@PathVariable("id") long id) {
 		return customerRepository.getOne(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable("id") long id) {
+		customerRepository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer, @PathVariable("id") long id) {
+		Optional<Customer> optionalCustomer = customerRepository.findById(id);
+		if(!optionalCustomer.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		customer.setId(id);
+		customerRepository.save(customer);
+		return ResponseEntity.noContent().build();
 	}
 	
 	
